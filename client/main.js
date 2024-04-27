79b27cbf1f00ee8ceda0564b4226bdab30325c71
ezP0tat0 <pselyem@gmail.com>
2024-04-27 18:05:51 +0200

    javitva(még nem jó)

--- client/main.js
+++ client/main.js
@@ -3,22 +3,30 @@ function goToLoginPage() {
 		
 }
 
-var connection = new ActiveXObject("ADODB.Connection") ;
 
-var connectionstring="Data Source=<server>;Initial Catalog=<catalog>;User ID=<user>;Password=<password>;Provider=SQLOLEDB";
 
-connection.Open(connectionstring);
-var rs = new ActiveXObject("ADODB.Recordset");
 
-rs.Open("SELECT * FROM table", connection);
-rs.MoveFirst
-while(!rs.eof)
-{
-   document.write(rs.fields(1));
-   rs.movenext;
-}
 
-rs.close;
-connection.close;
 
-document.getElementById("courses").innerHTML=fetchJSONData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
